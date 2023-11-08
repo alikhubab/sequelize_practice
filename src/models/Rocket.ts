157b81bdc56f36e5646b86fb59f088bc6c05e573
khubaib-ali-codegenio <khubaib.ali@codegenio.com>
2023-11-08 12:01:29 +0500

    Syncing a single model

--- src/models/Rocket.ts
+++ src/models/Rocket.ts
@@ -4,12 +4,17 @@ import { sequelize } from "../config/connection";
 
 class Rocket extends Model {
   declare id: number;
+  declare rocketName: string;
+  declare manufacturer: string;
+  declare fuelType: string;
+  declare payloadCapacity: number;
+  declare currentStatus: string;
 }
 
 Rocket.init(
   {
     id: {
-      type: DataTypes.NUMBER,
+      type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
     },
@@ -17,15 +22,25 @@ Rocket.init(
     manufacturer: DataTypes.STRING,
     fuelType: DataTypes.STRING,
     payloadCapacity: DataTypes.INTEGER,
+    currentStatus: DataTypes.STRING,
   },
   {
     sequelize,
     modelName: "Rocket",
-    freezeTableName: true,
-    tableName: "Rocket",
   }
 );
 
-const rocket = new Rocket();
-rocket.id;
+// Rocket.sync(); // This creates the table if it doesn't exist (and does nothing if it already exists)
+
+// const rocket = new Rocket();
+// rocket.id;
+// rocket.rocketName = "Yasin 105";
+// rocket.manufacturer = "Hamas";
+// rocket.fuelType = "Barood";
+// rocket.payloadCapacity = 1000;
+// rocket.save();
+
+// Rocket.sync({ force: true }); // This creates the table, dropping it first if it already existed
+// Rocket.sync({ alter: true }); // This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
+
 console.log(Rocket === sequelize.models.Rocket);
