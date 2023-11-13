ff0a8f0e0cde4a97b01ce816114730820e46dc35
khubaib-ali-codegenio <khubaib.ali@codegenio.com>
2023-11-13 12:25:23 +0500

    adding custom methods in model class

--- src/models/Rocket.ts
+++ src/models/Rocket.ts
@@ -8,8 +8,25 @@ class Rocket extends Model {
   declare manufacturer: string;
   declare fuelType: string;
   declare payloadCapacity: number;
-  declare currentStatus: string;
+  declare currentStatus: "active" | "production" | "inactive";
   declare barrelLength: number;
+
+  static getRocketsCount() {
+    return 1;
+  }
+
+  fire() {
+    return ">>***********>>";
+  }
+
+  getSepcs() {
+    return [
+      this.rocketName,
+      this.fuelType,
+      this.payloadCapacity,
+      this.barrelLength,
+    ].join(" ");
+  }
 }
 
 Rocket.init(
@@ -37,15 +54,28 @@ Rocket.init(
 // Rocket.sync(); // This creates the table if it doesn't exist (and does nothing if it already exists)
 
 // const rocket = new Rocket();
-// rocket.id;
-// rocket.rocketName = "Yasin 105";
+
+// rocket.rocketName = "Yasin 107";
 // rocket.manufacturer = "Hamas";
 // rocket.fuelType = "Barood";
 // rocket.payloadCapacity = 1000;
+// rocket.barrelLength = 110;
+// rocket.currentStatus = "active";
+
 // rocket.save();
 
 // Rocket.sync({ force: true }); // This creates the table, dropping it first if it already existed
-Rocket.sync({ alter: true }); // This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
+// Rocket.sync({ alter: true }); // This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 
 // Rocket.drop();
-console.log(Rocket === sequelize.models.Rocket);
+
+Rocket.findOne({
+  where: {
+    barrelLength: 110,
+  },
+}).then((rocket) => {
+  console.log(`${rocket?.rocketName} is Fired>>`, rocket?.fire());
+  console.log(`${rocket?.rocketName}'s specs>>`, rocket?.getSepcs());
+});
+
+// console.log(Rocket === sequelize.models.Rocket);
