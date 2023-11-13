e2e5cd86e54928feaf262a1c63efd60c08ec04b0
khubaib-ali-codegenio <khubaib.ali@codegenio.com>
2023-11-13 12:44:34 +0500

    using .build() method to create an instance

---
+++ src/models/Student.ts
@@ -0,0 +1,22 @@
+import { Sequelize, Model, DataTypes } from "sequelize";
+import { sequelize } from "../config/connection";
+
+interface StudentType extends Model {
+  name: string;
+}
+const Student = sequelize.define<StudentType>("student", {
+  name: DataTypes.TEXT,
+  favoriteColor: {
+    type: DataTypes.TEXT,
+    defaultValue: "green",
+  },
+  age: DataTypes.INTEGER,
+  cash: DataTypes.INTEGER,
+});
+
+(async () => {
+  await sequelize.sync({ force: true });
+  const ali = Student.build({ name: "Ali" });
+  console.log(ali.name);
+  ali.save();
+})();
