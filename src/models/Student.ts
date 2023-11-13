7eb0297f2ba89d20486176c194a12fa55d627c64
khubaib-ali-codegenio <khubaib.ali@codegenio.com>
2023-11-13 16:10:51 +0500

    reading whole of the table

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -1,6 +1,7 @@
 import { Sequelize, Model, DataTypes } from "sequelize";
 import { sequelize } from "../config/connection";
 import { json } from "stream/consumers";
+import { User } from "./user";
 
 interface StudentType extends Model {
   name: string;
@@ -17,33 +18,48 @@ const Student = sequelize.define<StudentType>("student", {
 });
 
 (async () => {
+  const students = await Student.findAll();
+  console.log(students.every((user) => user instanceof User));
+  console.log("All users>>", JSON.stringify(students, null, 2));
+
+  //   const hamza = await Student.create(
+  //     {
+  //       name: "Hamza",
+  //       age: 24,
+  //       favoriteColor: "magenta",
+  //     },
+  //     {
+  //       fields: ["name", "age"],
+  //     }
+  //   );
+
   //   await sequelize.sync({ force: true });
   //   const ali = Student.build({ name: "Ali" });
   //   console.log(ali.name);
   //   ali.save();
 
-  const amna = await Student.findOne({
-    where: {
-      age: 44,
-    },
-  });
-  if (amna) {
-    // await amna.increment("age");
-    await amna.increment({
-      age: 10,
-      cash: 33,
-    });
-    await amna.increment(["age", "cash"], {
-      by: 64,
-    });
-    // amna.name = "Amna Kanwal Khan";
-    // amna.age = 32;
-    // amna.save({ fields: ["name"] });
+  //   const amna = await Student.findOne({
+  //     where: {
+  //       age: 118,
+  //     },
+  //   });
+  //   if (amna) {
+  //     // await amna.increment("age");
+  //     await amna.increment({
+  //       age: 10,
+  //       cash: 33,
+  //     });
+  //     await amna.increment(["age", "cash"], {
+  //       by: 64,
+  //     });
+  // amna.name = "Amna Kanwal Khan";
+  // amna.age = 32;
+  // amna.save({ fields: ["name"] });
 
-    // await amna.reload();
-    // console.log(amna.age);
-    //   await amna.save();
-  }
+  // await amna.reload();
+  // console.log(amna.age);
+  //   await amna.save();
+  //   }
 
   //   const misbah = await Student.findOne({
   //     where: {
